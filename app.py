e04deb679fa1447d10be59b0559a780c0139019c
Christopher <cjszk@hotmail.com>
2018-05-09 19:52:58 -0700

    make it better

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_main():
-    return render_template('index.html')
+    return "Heroku Test"
+    # return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
